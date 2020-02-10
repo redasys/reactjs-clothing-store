5b869edf92f342a6366b4bcec93d7f0e0b412e4f
regan <regan@HPLAPTOPINOFFIC>
2020-02-10 14:30:04 -0500

    add grouped items to cart in reducer via util func

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,6 +1,10 @@
 import { cartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
-const INITIAL_STATE = { hidden: true };
+const INITIAL_STATE = { 
+    hidden: true, 
+    cartItems:[]
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
@@ -9,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             }
+            case cartActionTypes.ADD_ITEM:
+                return {
+                    ...state,
+                    cartItems: addItemToCart(state.cartItems, action.payload)
+                }
         default:
             return state;
 
---
+++ crwn-clothing/src/redux/cart/cart.utils.js
@@ -0,0 +1,12 @@
+export const addItemToCart = (cartItems, itemToAdd) => {
+    const existingItem = cartItems.find(i => i.id === itemToAdd.id);
+    if (existingItem){
+        return cartItems.map(i=>
+            i.id===itemToAdd.id
+            ? {...i, quantity: i.quantity + 1}
+            : i
+            );
+    }
+
+    return [...cartItems, {...itemToAdd, quantity: 1}];
+}
