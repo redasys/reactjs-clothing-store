dc2d0b57d449789086e27fd806846b9a93aae347
regan <regan@HPLAPTOPINOFFIC>
2020-02-10 15:26:21 -0500

    add items to cart

--- crwn-clothing/src/.components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/.components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,29 @@
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
 
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+        {
+            cartItems.map(item=>(
+                <CartItem key={item.id} item={item} />
+            ))
+        }
+        </div>
         <CustomButton >GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);
--- crwn-clothing/src/redux/cart/cart.utils.js
+++ crwn-clothing/src/redux/cart/cart.utils.js
@@ -10,3 +10,16 @@ export const addItemToCart = (cartItems, itemToAdd) => {
 
     return [...cartItems, {...itemToAdd, quantity: 1}];
 }
+
+export const removeItemFromCart = (cartItems, itemToAdd) => {
+    const existingItem = cartItems.find(i => i.id === itemToAdd.id);
+    if (existingItem && existingItem.quantity > 1){
+        return cartItems.map(i=>
+            i.id===itemToAdd.id
+            ? {...i, quantity: i.quantity - 1}
+            : i
+            );
+    }
+    cartItems.pop(itemToAdd);
+    return [...cartItems];
+}
