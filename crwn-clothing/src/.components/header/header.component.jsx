6e58b9b1d2323c2875aa74242274834e82d450e2
regan <regan@HPLAPTOPINOFFIC>
2020-02-10 10:59:18 -0500

    cart with redux

--- crwn-clothing/src/.components/header/header.component.jsx
+++ crwn-clothing/src/.components/header/header.component.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import './header.styles.scss';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => {
+
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to="/">
@@ -17,17 +20,23 @@ const Header = ({currentUser}) => {
                 <Link className='option' to='/shop'>SHOP</Link>
                 <Link className='option' to='/contact'>CONTACT</Link>
                 {
-                    currentUser?
-                    <div className='option' onClick={()=>auth.signOut()}>LOGOUT</div>
-                    :<Link className='option' to='/login'>LOGIN</Link> 
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>LOGOUT</div>
+                        : <Link className='option' to='/login'>LOGIN</Link>
                 }
-            </div>            
+                <CartIcon />
+            </div>
+            {
+                hidden ? null :
+                    <CartDropdown />
+            }
         </div>
     )
 };
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
