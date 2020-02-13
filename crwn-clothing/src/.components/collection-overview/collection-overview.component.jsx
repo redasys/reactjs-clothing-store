ed7edccf57f19c8530fedb2389ebc2783b107b88
regan <regan@HPLAPTOPINOFFIC>
2020-02-13 03:19:33 -0500

    Category page, data normalization, more redux

---
+++ crwn-clothing/src/.components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import './collection-overview.styles.scss';
+
+
+
+const CollectionOverview = ({collections}) => {
+    console.log('collections', collections)
+    return (        
+        <div className='collections-overview'>
+        <span>Hi there!</span>
+    {
+        collections.map(({id, ...others})=>(
+            <CollectionPreview key={id} {...others} />
+        ))
+    }
+    </div>)
+};
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
